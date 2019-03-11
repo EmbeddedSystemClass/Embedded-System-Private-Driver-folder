e73696870c75e6f1eec383d22e74fa31744cac6f
k.chi student <k.chi@student.uq.edu.au>
2019-03-11 22:17:52 +1000

    [mylib][stage1][lta1000g] Add inition version for LED BAR .

---
+++ lta1000g/s4527438_hal_lta1000g.c
@@ -0,0 +1,223 @@
+ /** 
+ **************************************************************
+ * @file mylib/sxxxxxx_mylib_template.c
+ * @author MyName - MyStudent ID
+ * @date 22022018
+ * @brief mylib template driver
+ * REFERENCE: DON'T JUST COPY THIS BLINDLY.pdf 
+ ***************************************************************
+ * EXTERNAL FUNCTIONS 
+ ***************************************************************
+ * sxxxxxx_mylib_template_init() - intialise mylib template driver
+ * sxxxxxx_mylib_template_set() - set mylib template driver 
+ *************************************************************** 
+ */
+/* Includes ------------------------------------------------------------------*/
+#include "board.h"
+#include "stm32f4xx_hal.h"
+#include "debug_printf.h"
+#include "s4527438_hal_lta1000g.h"
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+/**
+  * @brief General Purpose I/O
+  */
+
+typedef struct
+{
+  GPIO_TypeDef  *GPIO_PORT;      
+  uint16_t      GPIO_PIN;      
+  //uint32_t      RESERVED[236]; 
+} GPIO_PIN_Info;
+static GPIO_PIN_Info segment_to_gpio_pin_info[(LEDBAR_LED_END - LEDBAR_LED_START + 1)] = {0x00};
+
+static void lta1000g_seg_set(int segment,unsigned char segment_value);
+
+void s4527438_hal_lta1000g_init(void) {
+    GPIO_InitTypeDef  GPIO_InitStructure;
+
+    /* Enable the GPIO_LED Clock */
+    __LEDBAR_0_LED_GPIO_CLK();
+    __LEDBAR_1_LED_GPIO_CLK();
+    __LEDBAR_2_LED_GPIO_CLK();
+    __LEDBAR_3_LED_GPIO_CLK();
+    __LEDBAR_4_LED_GPIO_CLK();
+    __LEDBAR_5_LED_GPIO_CLK();
+    __LEDBAR_6_LED_GPIO_CLK();
+    __LEDBAR_7_LED_GPIO_CLK();
+    __LEDBAR_8_LED_GPIO_CLK();
+    __LEDBAR_9_LED_GPIO_CLK();
+
+    /* Configure the GPIO_LED pin */
+    GPIO_InitStructure.Pin = LEDBAR_0_LED_PIN;
+    GPIO_InitStructure.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStructure.Pull = GPIO_PULLUP;
+    GPIO_InitStructure.Speed = GPIO_SPEED_FAST;
+    HAL_GPIO_Init(LEDBAR_0_LED_GPIO_PORT, &GPIO_InitStructure);
+    segment_to_gpio_pin_info[LEDBAR_0_LED].GPIO_PORT = LEDBAR_0_LED_GPIO_PORT;
+    segment_to_gpio_pin_info[LEDBAR_0_LED].GPIO_PIN = LEDBAR_0_LED_PIN;
+
+    GPIO_InitStructure.Pin = LEDBAR_1_LED_PIN;
+    GPIO_InitStructure.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStructure.Pull = GPIO_PULLUP;
+    GPIO_InitStructure.Speed = GPIO_SPEED_FAST;
+    HAL_GPIO_Init(LEDBAR_1_LED_GPIO_PORT, &GPIO_InitStructure);
+    segment_to_gpio_pin_info[LEDBAR_1_LED].GPIO_PORT = LEDBAR_1_LED_GPIO_PORT;
+    segment_to_gpio_pin_info[LEDBAR_1_LED].GPIO_PIN = LEDBAR_1_LED_PIN;
+
+    GPIO_InitStructure.Pin = LEDBAR_2_LED_PIN;
+    GPIO_InitStructure.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStructure.Pull = GPIO_PULLUP;
+    GPIO_InitStructure.Speed = GPIO_SPEED_FAST;
+    HAL_GPIO_Init(LEDBAR_2_LED_GPIO_PORT, &GPIO_InitStructure);
+    segment_to_gpio_pin_info[LEDBAR_2_LED].GPIO_PORT = LEDBAR_2_LED_GPIO_PORT;
+    segment_to_gpio_pin_info[LEDBAR_2_LED].GPIO_PIN = LEDBAR_2_LED_PIN;
+
+    GPIO_InitStructure.Pin = LEDBAR_3_LED_PIN;
+    GPIO_InitStructure.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStructure.Pull = GPIO_PULLUP;
+    GPIO_InitStructure.Speed = GPIO_SPEED_FAST;
+    HAL_GPIO_Init(LEDBAR_3_LED_GPIO_PORT, &GPIO_InitStructure);
+    segment_to_gpio_pin_info[LEDBAR_3_LED].GPIO_PORT = LEDBAR_3_LED_GPIO_PORT;
+    segment_to_gpio_pin_info[LEDBAR_3_LED].GPIO_PIN = LEDBAR_3_LED_PIN;
+
+    GPIO_InitStructure.Pin = LEDBAR_4_LED_PIN;
+    GPIO_InitStructure.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStructure.Pull = GPIO_PULLUP;
+    GPIO_InitStructure.Speed = GPIO_SPEED_FAST;
+    HAL_GPIO_Init(LEDBAR_4_LED_GPIO_PORT, &GPIO_InitStructure);
+    segment_to_gpio_pin_info[LEDBAR_4_LED].GPIO_PORT = LEDBAR_4_LED_GPIO_PORT;
+    segment_to_gpio_pin_info[LEDBAR_4_LED].GPIO_PIN = LEDBAR_4_LED_PIN;
+
+    GPIO_InitStructure.Pin = LEDBAR_5_LED_PIN;
+    GPIO_InitStructure.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStructure.Pull = GPIO_PULLUP;
+    GPIO_InitStructure.Speed = GPIO_SPEED_FAST;
+    HAL_GPIO_Init(LEDBAR_5_LED_GPIO_PORT, &GPIO_InitStructure);
+    segment_to_gpio_pin_info[LEDBAR_5_LED].GPIO_PORT = LEDBAR_5_LED_GPIO_PORT;
+    segment_to_gpio_pin_info[LEDBAR_5_LED].GPIO_PIN = LEDBAR_5_LED_PIN;
+
+    GPIO_InitStructure.Pin = LEDBAR_6_LED_PIN;
+    GPIO_InitStructure.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStructure.Pull = GPIO_PULLUP;
+    GPIO_InitStructure.Speed = GPIO_SPEED_FAST;
+    HAL_GPIO_Init(LEDBAR_6_LED_GPIO_PORT, &GPIO_InitStructure);
+    segment_to_gpio_pin_info[LEDBAR_6_LED].GPIO_PORT = LEDBAR_6_LED_GPIO_PORT;
+    segment_to_gpio_pin_info[LEDBAR_6_LED].GPIO_PIN = LEDBAR_6_LED_PIN;
+
+    GPIO_InitStructure.Pin = LEDBAR_7_LED_PIN;
+    GPIO_InitStructure.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStructure.Pull = GPIO_PULLUP;
+    GPIO_InitStructure.Speed = GPIO_SPEED_FAST;
+    HAL_GPIO_Init(LEDBAR_7_LED_GPIO_PORT, &GPIO_InitStructure);
+    segment_to_gpio_pin_info[LEDBAR_7_LED].GPIO_PORT = LEDBAR_7_LED_GPIO_PORT;
+    segment_to_gpio_pin_info[LEDBAR_7_LED].GPIO_PIN = LEDBAR_7_LED_PIN;
+
+    GPIO_InitStructure.Pin = LEDBAR_8_LED_PIN;
+    GPIO_InitStructure.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStructure.Pull = GPIO_PULLUP;
+    GPIO_InitStructure.Speed = GPIO_SPEED_FAST;
+    HAL_GPIO_Init(LEDBAR_8_LED_GPIO_PORT, &GPIO_InitStructure);
+    segment_to_gpio_pin_info[LEDBAR_8_LED].GPIO_PORT = LEDBAR_8_LED_GPIO_PORT;
+    segment_to_gpio_pin_info[LEDBAR_8_LED].GPIO_PIN = LEDBAR_8_LED_PIN;
+
+    GPIO_InitStructure.Pin = LEDBAR_9_LED_PIN;
+    GPIO_InitStructure.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStructure.Pull = GPIO_PULLUP;
+    GPIO_InitStructure.Speed = GPIO_SPEED_FAST;
+    HAL_GPIO_Init(LEDBAR_9_LED_GPIO_PORT, &GPIO_InitStructure);
+    segment_to_gpio_pin_info[LEDBAR_9_LED].GPIO_PORT = LEDBAR_9_LED_GPIO_PORT;
+    segment_to_gpio_pin_info[LEDBAR_9_LED].GPIO_PIN = LEDBAR_9_LED_PIN;
+
+}
+
+static void lta1000g_seg_set(int segment,unsigned char segment_value) {
+
+    GPIO_PIN_Info  *gpio_info = (&(segment_to_gpio_pin_info[segment]));
+    GPIO_TypeDef   *gpio_port =  gpio_info->GPIO_PORT;      
+
+    if( segment_value == 1 ) {
+        gpio_port->BSRR |= gpio_info->GPIO_PIN;
+    } else if( segment_value == 0 ) {
+        gpio_port->BSRR |= (gpio_info->GPIO_PIN) << 16;
+    }
+#if 0
+    if ((ledmask & LEDBAR_0_LEDMASK) != 0) {
+        BRD_GREEN_LED_GPIO_PORT->BSRR |= BRD_GREEN_LED_PIN;
+    }
+
+    if ((ledmask & BRD_BLUE_LEDMASK) != 0) {
+        BRD_BLUE_LED_GPIO_PORT->BSRR |= BRD_BLUE_LED_PIN;
+    }
+
+    if ((ledmask & BRD_RED_LEDMASK) != 0) {
+        BRD_RED_LED_GPIO_PORT->BSRR |= BRD_RED_LED_PIN;
+    }
+#endif
+}
+
+void s4527438_hal_lta1000g_write(unsigned short value) {
+    if ((value & LEDBAR_0_LEDMASK) != 0) {
+        lta1000g_seg_set(0,1);
+    }else {
+        lta1000g_seg_set(0,0);
+    }
+
+    if ((value & LEDBAR_1_LEDMASK) != 0) {
+        lta1000g_seg_set(1,1);
+    }else {
+        lta1000g_seg_set(1,0);
+    }
+
+    if ((value & LEDBAR_2_LEDMASK) != 0) {
+        lta1000g_seg_set(2,1);
+    }else {
+        lta1000g_seg_set(2,0);
+    }
+
+    if ((value & LEDBAR_3_LEDMASK) != 0) {
+        lta1000g_seg_set(3,1);
+    }else {
+        lta1000g_seg_set(3,0);
+    }
+
+    if ((value & LEDBAR_4_LEDMASK) != 0) {
+        lta1000g_seg_set(4,1);
+    }else {
+        lta1000g_seg_set(4,0);
+    }
+
+    if ((value & LEDBAR_5_LEDMASK) != 0) {
+        lta1000g_seg_set(5,1);
+    }else {
+        lta1000g_seg_set(5,0);
+    }
+
+    if ((value & LEDBAR_6_LEDMASK) != 0) {
+        lta1000g_seg_set(6,1);
+    }else {
+        lta1000g_seg_set(6,0);
+    }
+
+    if ((value & LEDBAR_7_LEDMASK) != 0) {
+        lta1000g_seg_set(7,1);
+    }else {
+        lta1000g_seg_set(7,0);
+    }
+
+    if ((value & LEDBAR_8_LEDMASK) != 0) {
+        lta1000g_seg_set(8,1);
+    }else {
+        lta1000g_seg_set(8,0);
+    }
+
+    if ((value & LEDBAR_9_LEDMASK) != 0) {
+        lta1000g_seg_set(9,1);
+    }else {
+        lta1000g_seg_set(9,0);
+    }
+}
---
+++ lta1000g/s4527438_hal_lta1000g.h
@@ -0,0 +1,91 @@
+/** 
+ **************************************************************
+ * @file mylib/sxxxxxx_mylib_template.h
+ * @author MyName - MyStudent ID
+ * @date 22022018
+ * @brief mylib template driver
+ * REFERENCE: DON'T JUST COPY THIS BLINDLY.pdf 
+ ***************************************************************
+ * EXTERNAL FUNCTIONS 
+ ***************************************************************
+ * sxxxxxx_mylib_template_init() - intialise mylib template driver
+ * sxxxxxx_mylib_template_set() - set mylib template driver 
+ *************************************************************** 
+ */
+
+#ifndef S4527438_HAL_LTA1000G_H_
+#define S4527438_HAL_LTA1000G_H_
+
+/**********************  BOARD LEDs  *****************************/
+
+//LED definitions (Left ~ Right : D0 ~ D9)
+#define LEDBAR_0_LED                        0   //D0 : PG9
+#define LEDBAR_0_LEDMASK                    1 << LEDBAR_0_LED
+#define LEDBAR_0_LED_PIN                    GPIO_PIN_9
+#define LEDBAR_0_LED_GPIO_PORT              GPIOG
+#define __LEDBAR_0_LED_GPIO_CLK()           __GPIOG_CLK_ENABLE()
+
+#define LEDBAR_1_LED                        1  //D1 : PG14
+#define LEDBAR_1_LEDMASK                    1 << LEDBAR_0_LED
+#define LEDBAR_1_LED_PIN                    GPIO_PIN_14
+#define LEDBAR_1_LED_GPIO_PORT              GPIOG
+#define __LEDBAR_1_LED_GPIO_CLK()           __GPIOG_CLK_ENABLE()
+
+#define LEDBAR_2_LED                        2   //D2 : PF15
+#define LEDBAR_2_LEDMASK                    1 << LEDBAR_0_LED
+#define LEDBAR_2_LED_PIN                    GPIO_PIN_15
+#define LEDBAR_2_LED_GPIO_PORT              GPIOF
+#define __LEDBAR_2_LED_GPIO_CLK()           __GPIOF_CLK_ENABLE()
+
+#define LEDBAR_3_LED                        3   //D3 : PE13
+#define LEDBAR_3_LEDMASK                    1 << LEDBAR_0_LED
+#define LEDBAR_3_LED_PIN                    GPIO_PIN_13
+#define LEDBAR_3_LED_GPIO_PORT              GPIOE
+#define __LEDBAR_3_LED_GPIO_CLK()           __GPIOE_CLK_ENABLE()
+
+#define LEDBAR_4_LED                        4   //D4 : PF14
+#define LEDBAR_4_LEDMASK                    1 << LEDBAR_0_LED
+#define LEDBAR_4_LED_PIN                    GPIO_PIN_14
+#define LEDBAR_4_LED_GPIO_PORT              GPIOF
+#define __LEDBAR_4_LED_GPIO_CLK()           __GPIOF_CLK_ENABLE()
+
+#define LEDBAR_5_LED                        5   //D5 : PE11
+#define LEDBAR_5_LEDMASK                    1 << LEDBAR_0_LED
+#define LEDBAR_5_LED_PIN                    GPIO_PIN_11
+#define LEDBAR_5_LED_GPIO_PORT              GPIOE
+#define __LEDBAR_5_LED_GPIO_CLK()           __GPIOE_CLK_ENABLE()
+
+#define LEDBAR_6_LED                        6   //D6 : PE9
+#define LEDBAR_6_LEDMASK                    1 << LEDBAR_0_LED
+#define LEDBAR_6_LED_PIN                    GPIO_PIN_9
+#define LEDBAR_6_LED_GPIO_PORT              GPIOE
+#define __LEDBAR_6_LED_GPIO_CLK()           __GPIOE_CLK_ENABLE()
+
+#define LEDBAR_7_LED                        7   //D7 : PF13
+#define LEDBAR_7_LEDMASK                    1 << LEDBAR_0_LED
+#define LEDBAR_7_LED_PIN                    GPIO_PIN_13
+#define LEDBAR_7_LED_GPIO_PORT              GPIOF
+#define __LEDBAR_7_LED_GPIO_CLK()           __GPIOF_CLK_ENABLE()
+
+#define LEDBAR_8_LED                        8   //D8 : PF12
+#define LEDBAR_8_LEDMASK                    1 << LEDBAR_0_LED
+#define LEDBAR_8_LED_PIN                    GPIO_PIN_12
+#define LEDBAR_8_LED_GPIO_PORT              GPIOF
+#define __LEDBAR_8_LED_GPIO_CLK()           __GPIOF_CLK_ENABLE()
+
+#define LEDBAR_9_LED                        9   //D9 : PD15
+#define LEDBAR_9_LEDMASK                    1 << LEDBAR_0_LED
+#define LEDBAR_9_LED_PIN                    GPIO_PIN_15
+#define LEDBAR_9_LED_GPIO_PORT              GPIOD
+#define __LEDBAR_9_LED_GPIO_CLK()           __GPIOD_CLK_ENABLE()
+
+#define LEDBAR_LED_START                    LEDBAR_0_LED
+#define LEDBAR_LED_END                      LEDBAR_9_LED
+
+#define LEDBAR_LEDMASK_START                LEDBAR_0_LEDMASK
+#define LEDBAR_LEDMASK_END                  LEDBAR_9_LEDMASK
+
+void s4527438_hal_lta1000g_init();
+void s4527438_hal_lta1000g_write(unsigned short value);
+
+#endif /* S4527438_HAL_LTA1000G_H_ */
