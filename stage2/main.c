1a9c0f1b52309868df4416706dfef608042dc9a5
k.chi student <k.chi@student.uq.edu.au>
2019-03-24 18:38:45 +1000

    [stage2] Add command function and mode switch.

--- stage2/main.c
+++ stage2/main.c
@@ -16,12 +16,20 @@
 
 /* Private typedef -----------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
-#define MAIN_LOOP_POLLING_DELAY         100 //500 ms
+#define MAIN_LOOP_POLLING_DELAY         125 //500 ms
+#define METRONOME_MODE                  1
+#define NORMAL_MODE                     0 
 
 
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
+static int atimer_period_ms = S4527438_HAL_ATIMER_PERIOD;
+static int atimer_clk = S4527438_HAL_ATIMER_CLKSPEED;
+static int cur_mode = NORMAL_MODE;
 /* Private function prototypes -----------------------------------------------*/
+static void handle_normal_mode(int cmd);
+static void handle_metronome_mode(int cmd);
+
 void Hardware_init(void);
 
 /**
@@ -35,28 +43,91 @@ int main(void)  {
 	Hardware_init();	//Initalise hardware modules
 	
     char RxChar;
-    int cur_angle = 0;
 
 	/* Main processing loop */
     while (1) {
         RxChar = debug_getc();
-		//HAL_Delay(MAIN_LOOP_POLLING_DELAY);		//Delay for 2.5s
 
         if( RxChar != '\0' ) {
-            //debug_printf("%c\n", RxChar);
-
-            cur_angle = s4527438_hal_pantilt_pan_read();
-            if( RxChar == '+' ) {
-                s4527438_hal_pantilt_pan_write(++cur_angle);
-            } else if( RxChar == '-' ) {
-                s4527438_hal_pantilt_pan_write(--cur_angle);
+            switch(RxChar) {
+                case 'm':
+                    cur_mode = METRONOME_MODE;
+                    break;
+                case 'n':
+                    cur_mode = NORMAL_MODE;
+                    break;
+                default:
+                    if( cur_mode == NORMAL_MODE ) {
+                        handle_normal_mode(RxChar);
+                    } else {
+                        handle_metronome_mode(RxChar);
+                    }
+                    break;
             }
-            //debug_printf("%d\n", cur_angle);
+
+		    HAL_Delay(MAIN_LOOP_POLLING_DELAY);		//Delay for 2.5s
         }
         HAL_Delay(125);
 	}
 
     return 0;
+}
+
+static void handle_normal_mode(int cmd) {
+    int cur_angle = 0;
+    int atimer_counter = 0;
+    int atimer_counter_ms = 0;
+
+    switch(cmd) {
+        case 'f':
+            s4527438_hal_atimer_timer_pause();
+            break;
+        case 'r':
+            s4527438_hal_atimer_timer_resume();
+            break;
+        case 'z':
+            s4527438_hal_atimer_timer_reset();
+            break;
+        case 'c':
+            atimer_counter = s4527438_hal_atimer_timer_read();
+            debug_printf("atimer_counter = %d\n", atimer_counter);
+            break;
+        case 't':
+            atimer_counter_ms = s4527438_hal_atimer_timer_getms();
+            debug_printf(" atimer_counter_ms = %d\n", atimer_counter_ms);
+            break;
+
+                
+        case '+':
+            atimer_period_ms += 10;
+            s4527438_hal_atimer_period_set(atimer_period_ms);
+            break;
+        case '-':
+            atimer_period_ms -= 10;
+            s4527438_hal_atimer_period_set(atimer_period_ms);
+            break;
+        case 'i':
+            atimer_clk += 1000;
+            s4527438_hal_atimer_clkspeed_set(atimer_clk);
+            break;
+        case 'd':
+            atimer_clk -= 1000;
+            s4527438_hal_atimer_clkspeed_set(atimer_clk);
+            break;
+
+        case 'p':
+            cur_angle = s4527438_hal_pantilt_pan_read();
+            s4527438_hal_pantilt_pan_write(++cur_angle);
+            break;
+        case 'n':
+            cur_angle = s4527438_hal_pantilt_pan_read();
+            s4527438_hal_pantilt_pan_write(--cur_angle);
+            break;
+    }
+
+}
+
+static void handle_metronome_mode(int cmd) {
 }
 
 /**
